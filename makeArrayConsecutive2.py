c8881d395a511b3470cace6e10ee941360975d27
Maya Al Hourani <alhomaya735@gmail.com>
2023-10-04 12:09:33 -0400

    makeArrayConsecutive2 - CodeSignal Problem

---
+++ makeArrayConsecutive2.py
@@ -0,0 +1,14 @@
+'''
+Ratiorg got statues of different sizes as a present from CodeMaster for his birthday, each statue having an non-negative integer size. Since he likes to make things perfect, he wants to arrange them from smallest to largest so that each statue will be bigger than the previous one exactly by 1. He may need some additional statues to be able to accomplish that. Help him figure out the minimum number of additional statues needed.
+
+Example: For statues = [6, 2, 3, 8], the output should be solution(statues) = 3. Ratiorg needs statues of sizes 4, 5 and 7.
+'''
+
+def solution(statues):
+    statues.sort()
+    counter = 0
+    for i in range(0, len(statues)-1):
+        if (statues[i+1] - statues[i]) != 1:
+            counter += statues[i+1] - statues[i] - 1
+
+    return counter
